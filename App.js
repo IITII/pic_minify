42e1331610d023aadbc6fff5272304da9f74ac6c
IITII <ccmejx@gmail.com>
2023-06-13 05:38:27 +0000

    ref: dfs convert to save tmp space and disk io

--- App.js
+++ App.js
@@ -5,17 +5,17 @@
 'use strict'
 const fs = require('fs'),
   path = require('path'),
-  {mapLimit} = require('async')
+  { mapLimit } = require('async')
 
 const cwebp = require('./libs/cwebp.lib')
-const {spendTime, time_human} = require('./libs/utils.lib')
-const {copyFileSync, removePre} = require('./libs/file_utils.lib')
-const {readdir, convertPath, randomFileName} = require('./libs/file_utils.lib')
+const { spendTime, time_human } = require('./libs/utils.lib')
+const { copyFileSync, removePre } = require('./libs/file_utils.lib')
+const { readdir, convertPath, randomFileName } = require('./libs/file_utils.lib')
 
 const config = require('./config'),
   logger = require('./libs/logger.lib'),
   cacheDir = path.resolve(config.cacheDir, './pic_minify_cache')
-const {size_human} = require('./libs/utils.lib.js')
+const { size_human } = require('./libs/utils.lib.js')
 
 async function main(input, output) {
   if (!fs.statSync(input).isDirectory()) {
@@ -47,7 +47,7 @@ async function main(input, output) {
   }
   const copyFiles = files.files.map(_ => convertPath(input, _, output, true, true))
   const cacheFiles = copyFiles.map(_ => {
-    const {input, output} = _
+    const { input, output } = _
     return {
       input, output,
       cache: [randomFileName(input, cacheDir)],
@@ -67,7 +67,7 @@ async function main(input, output) {
     })))
     // .then(r => logger.debug(r))
     .then(_ => logger.info(`Remove cache dir: ${cacheDir}`))
-    .then(_ => fs.rmSync(cacheDir, {recursive: true}))
+    .then(_ => fs.rmSync(cacheDir, { recursive: true }))
 }
 
 async function replace_local(input, output) {
@@ -77,23 +77,107 @@ async function replace_local(input, output) {
   const files = await spendTime(`Readdir ${input}`, readdir, input, config.iRegex, config.minSize, logger)
   const copyFiles = files.files.map(_ => convertPath(input, _, input, true, true))
   const cacheFiles = copyFiles.map(_ => {
-    const {input, output} = _
+    const { input, output } = _
     return {
       input, output,
       cache: [randomFileName(input, cacheDir)],
     }
   })
-  return await spendTime(`Convert ${cacheFiles.length} files finish`, convert, cacheFiles)
-    .then(res => Promise.allSettled(res.map(r => {
-      const {input, output, cache} = r
-      const from = arrLast(cache)
-      return Promise.resolve()
-        .then(_ => spendTime(`Remove input file ${input}`, removePre, input, output))
-        .then(_ => spendTime(`Copy converted file ${from} to ${output}`, copyFileSync, from, output))
-    })))
+  return await spendTime(`Convert ${cacheFiles.length} files finish`, convertDfs, cacheFiles)
+    .then(res => {
+      return Promise.allSettled(res.map(r => {
+        const { input, output, cache } = r
+        const from = arrLast(cache)
+        return Promise.resolve()
+          .then(_ => spendTime(`Remove input file ${input}`, removePre, input, output))
+          .then(_ => spendTime(`Copy converted file ${from} to ${output}`, copyFileSync, from, output))
+      }))
+    })
     // .then(r => logger.debug(r))
     .then(_ => logger.info(`Remove cache dir: ${cacheDir}`))
-    .then(_ => fs.rmSync(cacheDir, {recursive: true}))
+    .then(_ => fs.rmSync(cacheDir, { recursive: true }))
+}
+
+async function convertDfs(cacheFiles) {
+  if (cacheFiles.length === 0) return
+  let items = [].concat(cacheFiles), res = []
+  let handled = 0, remaining, total = items.length, starts = [Date.now()], oriInSize = 0, outSize = 0
+  async function dfs(item) {
+    if (item.cache.length > 3) {
+      logger.info('cache length > 3')
+      return Promise.resolve(item)
+    }
+    const input = arrLast(item.cache),
+      output = randomFileName(input, cacheDir, 'webp')
+    if (item.cache.length === 1 && !fs.existsSync(input)) {
+      await spendTime(`Copy ${item.input} to cache ${path.basename(input)}`, copyFileSync, item.input, input)
+    }
+    return await spendTime(`Minify ${input} to ${path.basename(output)}`, cwebp, input, output)
+      .then(r => {
+        handled += 1
+        remaining = total - handled
+        let totalTime = Date.now() - starts[0]
+        let remainingTime = time_human(totalTime / total * remaining)
+        logger.info(`Files handled/total: ${handled}/${total}, remaining: ` +
+          `${remaining}, total time: ${time_human(totalTime)}, remaining time: ${remainingTime}`)
+        return r[0]
+      })
+      .then(async cwebpRes => {
+        const input1 = cwebpRes.input,
+          output1 = cwebpRes.output
+        const inputSize = fs.statSync(input1).size,
+          outputSize = fs.statSync(output1).size
+        let target
+        // 是否大于限制大小
+        if (outputSize > config.minSize) {
+          // continue
+          const skipLarge = config.skipIfLarge && outputSize > inputSize
+          // 每个文件最多处理次数: (0, maxDepth]
+          if (item.cache.length >= config.maxDepth) {
+            // final end
+            // 达到处理限制，按规则选对应的文件
+            target = skipLarge ? input1 : output1
+          } else if (skipLarge) {
+            // process end
+            // 处理后文件变大，不继续处理，选处理前文件
+            logger.debug(`Skip ${input1} because it's larger than original`)
+            target = input1
+          } else {
+            // continue
+            // 处理后文件过大，继续处理
+            target = output1
+            item.cache.push(target)
+
+            total += 1
+            return await dfs(item)
+          }
+        } else {
+          // end
+          target = output1
+        }
+        item.cache.push(target)
+        removePreTmpFiles(item.cache, target)
+        return Promise.resolve(item)
+      })
+      .catch(e => {
+        logger.info(e.message)
+        logger.debug(e)
+        // 可能是因为图片分辨率太大导致的, 这种情况下使用原图片即可
+        // 存在重复复制的情况
+        return Promise.resolve(item)
+      })
+      .then(item => {
+        if (item && typeof item === 'object' && item.output) {
+          res.push(item)
+        } else {
+          logger.warn(`ignore unknown item: ${item}`)
+        }
+      })
+  }
+  await mapLimit(items, config.mapLimit, async (item, cb) => {
+    return await dfs(item).catch(e => logger.error(e)).finally(cb)
+  })
+  return res
 }
 
 async function convert(cacheFiles) {
